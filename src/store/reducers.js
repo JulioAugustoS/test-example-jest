b7878d875ff259a004476f13d34947a8043e3472
Julio Augusto <julio@lapse.com.br>
2019-03-24 20:57:35 -0300

    Instalado e configurado redux para test

---
+++ src/store/reducers.js
@@ -0,0 +1,9 @@
+const initialState = [
+  { id: 0, text: 'Fazer café' },
+  { id: 1, text: 'Acessar o slack' },
+  { id: 2, text: 'Tomar café' }
+]
+
+export default function todos(state = initialState, action){
+  return state;
+}
